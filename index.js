1a4e0c089aba1900806e9357a90c1fe3e98b08fc
aryabodda4567 <aryabodda@gmail.com>
2026-02-12 22:59:39 +0530

    vercel config

--- index.js
+++ index.js
@@ -12,11 +12,10 @@ app.use(trackingRoutes);
 app.use(emailRoutes);
 
 if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-  });
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+        console.log(`Server running on http://localhost:${PORT}`);
+    });
 }
 
 module.exports = app;
-
