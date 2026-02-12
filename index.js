841c2c68700d9dfc3e858e9dcde26d141a7dc90a
aryabodda4567 <aryabodda@gmail.com>
2026-02-12 22:48:20 +0530

    vercel config added

--- index.js
+++ index.js
@@ -3,15 +3,20 @@ const express = require("express");
 
 const trackingRoutes = require("./track/trackingRoutes");
 const emailRoutes = require("./email/emailRoutes");
+
 const app = express();
+
 app.use(express.json());
 
 app.use(trackingRoutes);
 app.use(emailRoutes);
 
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+}
 
+module.exports = app;
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
