54a4a26fa39eadf894e4fe73f467de4bd992e40a
aryabodda4567 <aryabodda@gmail.com>
2026-02-13 10:40:47 +0530

    cors middleware custme code added

--- index.js
+++ index.js
@@ -6,11 +6,17 @@ const emailRoutes = require("./email/emailRoutes");
 
 const app = express();
 
-// Allow ALL origins (testing only)
-app.use(cors());
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-// Optional but good practice for preflight
-app.options("*", cors());
+    if (req.method === "OPTIONS") {
+        return res.status(200).end();
+    }
+
+    next();
+});
 
 app.use(express.json());
 
