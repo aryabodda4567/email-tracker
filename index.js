4c794e5acb3dd5240019ff7ec8c104812593d7a0
aryabodda4567 <aryabodda@gmail.com>
2026-02-13 10:13:08 +0530

    cors allowing to 127.0.0.1

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: "http://localhost:5500", // your frontend origin
+    origin: "http://127.0.0.1:5500", // your frontend origin
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: ["Content-Type"]
 }));
