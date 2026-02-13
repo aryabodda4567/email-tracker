c0cda5c4c44477beca3f98658fea01e2ff5871ca
aryabodda4567 <aryabodda@gmail.com>
2026-02-13 10:29:56 +0530

    cors bug fix

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ const emailRoutes = require("./email/emailRoutes");
 
 const app = express();
 
-// app.use(cors());
-app.use(express.json());
-
 // Allow ALL origins (testing only)
 app.use(cors());
 
 // Optional but good practice for preflight
 app.options("*", cors());
+
+app.use(express.json());
+
 
 app.use(trackingRoutes);
 app.use(emailRoutes);
